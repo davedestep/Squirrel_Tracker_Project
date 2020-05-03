b3663d625cfec7f2179e42418ebed418ca3eebc6
Dave <davedestep@gmail.com>
2020-05-03 22:02:10 +0000

    Add models

--- track/models.py
+++ track/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.utils.translation import gettext as _
+
+class Sighting(models.Model):
+
+    id = models.CharField(
+        help_text=_('Unique Squirrel ID'),
+        max_length=255,
+        primary_key=True,
+    )
+    
+    AGE_CHOICES = (
+        ('adult', 'Adult'),
+        ('juvenile', 'Juvenile'),
+    )
+
+    age = models.CharField('Age', max_length=50, choices=AGE_CHOICES)
+
+    latitude = models.DecimalField(
+        help_text=_('Latitude'),
+        max_digits=20,
+        decimal_places=10,
+    )
+
+    longitude = models.DecimalField(
+        help_text=_('Longitude'),
+        max_digits=20,
+        decimal_places=10,
+    )
+
+    date = models.DateField(
+        help_text=_('Sighting Date'),
+    )
+
+    def __str__(self):
+                return f'{self.id}: {self.name}'
+
 
 # Create your models here.
