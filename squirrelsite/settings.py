48ed42b7582d367a3af0b884d6f108f594cfa8eb
acg2176 <annamcguerrero@gmail.com>
2020-05-07 15:57:09 -0400

    update settings.py

--- squirrelsite/settings.py
+++ squirrelsite/settings.py
@@ -31,13 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'map.apps.MapConfig',
+    'track.apps.TrackConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'track',
+    
 ]
 
 MIDDLEWARE = [
