c1d17f78bc75a754f798a17d777039e4ffba4628
David DeStephano <davedestep@gmail.com>
2020-05-08 18:46:49 -0400

    changing path in sightings.urls
    should probably just move to map.urls

--- sightings/urls.py
+++ sightings/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('', views.home_view, name='home'),
+        path('map/', views.home_view, name='home'),
         path('', views.list_squirrel_sightings, name='sqirrels'),
 ]
 
