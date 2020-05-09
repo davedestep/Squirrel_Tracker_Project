78bf7db87ff00c2f7e507a11f89689299371a450
David DeStephano <davedestep@gmail.com>
2020-05-08 20:30:34 -0400

    Update urls.py

--- map/urls.py
+++ map/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('', views.home_view, name = 'home'),
-	path('', views.list_squirrel_sightings, name='squirrels'),
+	path('squirrels/', views.list_squirrel_sightings),
 
 ]
