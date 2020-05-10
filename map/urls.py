aeee7cf01acc8d968bfef1b76f265fe255227e68
acg2176 <annamcguerrero@gmail.com>
2020-05-10 19:52:13 -0400

    edit map/urls.py

--- map/urls.py
+++ map/urls.py
@@ -4,8 +4,9 @@ from . import views
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('', views.home_view, name = 'home'),
-	path('sightings/', views.list_squirrel_sightings),
-	path('stats/', views.squirrel_stats),
-	path('sightings/<squirrel_id>/', views.update),
-	path('sightings/add/', views.add_sighting),
+	#path('sightings/', views.list_squirrel_sightings),
+	#path('stats/', views.squirrel_stats),
+	# path('sightings/<squirrel_id>/', views.update),
+	# path('sightings/add/', views.add_sighting),
+	
 ]
