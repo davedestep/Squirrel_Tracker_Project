33bc10cdcad319a34e64a0cf1cc737d6a2a6d28e
acg2176 <annamcguerrero@gmail.com>
2020-05-10 15:08:05 -0400

    add nav bars

--- map/urls.py
+++ map/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('', views.home_view, name = 'home'),
-	path('squirrels/', views.list_squirrel_sightings),
+	path('sightings/', views.list_squirrel_sightings),
 	path('stats/', views.squirrel_stats),
 ]
