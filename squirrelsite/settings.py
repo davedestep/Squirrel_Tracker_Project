7028bb952493135e8039b6ddb336a4f31e831ce2
acg2176 <annamcguerrero@gmail.com>
2020-05-11 13:24:38 -0400

    delete settings app from settings.py

--- squirrelsite/settings.py
+++ squirrelsite/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'map.apps.MapConfig',
-    'sightings.apps.SightingsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
